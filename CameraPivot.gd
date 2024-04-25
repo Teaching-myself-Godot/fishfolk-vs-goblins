93225660c7cff27610ae1cfd13aa9b294ed57396
rene.vanderark@kb.nl <rene.vanderark@kb.nl>
2024-04-25 11:12:41 +0200

    adds 2d blend space for animations.

--- CameraPivot.gd
+++ CameraPivot.gd
@@ -7,10 +7,11 @@ func _process(_delta):
 		position_sum += g.position
 
 	var goblin_center = Vector3.ZERO if goblins.is_empty() else position_sum / goblins.size()
-	var d = 0.0
+	var max_d = 0.0
+	var max_y = 0.0
 	for g in goblins:
-		d = max(d, g.position.distance_to(goblin_center))
-	
+		max_d = max(max_d, g.position.distance_to(goblin_center))
+		max_y = max(max_y, g.position.y)
 	position.x = goblin_center.x
-	position.z = goblin_center.z + 18  + d * 1.5
-	position.y = 16 + d 
+	position.z = goblin_center.z + 9  + max_d * 1.5
+	position.y = 8 + max_y + max_d 
--- goblin.gd
+++ goblin.gd
@@ -1,8 +1,8 @@
 extends CharacterBody3D
 
 var speed = 0
-const MAX_SPEED = 6
-const JUMP_VELOCITY = 4.5
+const MAX_SPEED = 8
+const JUMP_VELOCITY = 9
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/3d/default_gravity")
@@ -37,12 +37,16 @@ func get_input_dir():
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y -= gravity * delta
+		velocity.y -= gravity * 3 * delta
 
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = get_input_dir()
+	if Input.is_action_pressed("shift"):
+		input_dir *= .25
 	var force = Vector2.ZERO.distance_to(input_dir)
 	var direction = Vector3(input_dir.x, 0, input_dir.y).normalized()
 
@@ -55,7 +59,12 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, 0, speed)
 		velocity.z = move_toward(velocity.z, 0, speed)
 
-	$AnimationTree.set("parameters/BlendSpace1D/blend_position", force)
+	var jump_force = 0 if is_on_floor() else velocity.y
+	if velocity.y < 1 and not is_on_floor():
+		jump_force = 1
+
+	$AnimationTree.set("parameters/BlendSpace2D/blend_position", Vector2(force, jump_force))
+
 	move_and_slide()
 
 func _on_static_body_3d_input_event(camera, event, pos, normal, shape_idx):
