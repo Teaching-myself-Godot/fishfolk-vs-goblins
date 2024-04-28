415fe29d4183a8a8f2c7adab71bd17583ace807c
rene.vanderark@kb.nl <rene.vanderark@kb.nl>
2024-04-28 23:48:25 +0200

    adds menu options, reusable blink shader.

--- scripts/debug.gd
+++ scripts/debug.gd
@@ -1,7 +1,7 @@
 extends Node
 
 var GoblinScene = preload("res://goblin.scn")
-var adding_state = []
+var goblin_map = {}
 
 func _unhandled_input(_event):
 	if Input.is_action_just_pressed("quit") and get_tree().get_nodes_in_group("goblins").is_empty():
@@ -13,37 +13,34 @@ func _unhandled_input(_event):
 		else:
 			DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 		
-	if Input.is_action_just_released("start-k") and no_goblin(0):
+	if Input.is_action_just_released("start-k"):
 		add_goblin_to_scene(0)
-	if Input.is_action_just_released("start-0") and no_goblin(1):
+	if Input.is_action_just_released("start-0"):
 		add_goblin_to_scene(1)
-	if Input.is_action_just_released("start-1") and no_goblin(2):
+	if Input.is_action_just_released("start-1"):
 		add_goblin_to_scene(2)
-	if Input.is_action_just_released("start-2") and no_goblin(3):
+	if Input.is_action_just_released("start-2"):
 		add_goblin_to_scene(3)
-	if Input.is_action_just_released("start-3") and no_goblin(4):
+	if Input.is_action_just_released("start-3"):
 		add_goblin_to_scene(4)
 
 func add_goblin_to_scene(num : int):
-	adding_state.append(num)
+	if num in goblin_map:
+		if goblin_map[num] and is_instance_valid(goblin_map[num]):
+			return
+		else: 
+			goblin_map.erase(num)
+		
 	var new_goblin = GoblinScene.instantiate()
+	goblin_map[num] = new_goblin
 	new_goblin.player_num = num
 	new_goblin.position = Vector3(0, 4, 0)
 
 	var goblin = get_tree().get_first_node_in_group("goblins")
 	if goblin and is_instance_valid(goblin):
-		new_goblin.position.x = goblin.position.x + (1 + randf()) * 2
-		new_goblin.position.z = goblin.position.z + (1 + randf()) * 2
+		new_goblin.position.x = goblin.position.x + (-2 + randf() * 4)
+		new_goblin.position.z = goblin.position.z + (-2 + randf() * 4)
 		new_goblin.position.y = goblin.position.y + 4
 
 	add_child.call_deferred(new_goblin)
-	adding_state.erase(num)
 
-func no_goblin(num : int) -> bool:
-	if num in adding_state:
-		return false
-	var goblins = get_tree().get_nodes_in_group("goblins")
-	for g in goblins:
-		if g.player_num == num:
-			return false
-	return true
--- scripts/tree_context_menu.gd
+++ scripts/tree_context_menu.gd
@@ -4,8 +4,14 @@ var is_open = false
 
 const MENU_RADIUS = 200.0
 
+var type_options = []
+var type_labels = {}
+
 func _ready():
 	$BackdropCircle.radius = MENU_RADIUS
+	type_options = $TypeMenu.find_children("*-option")
+	for type_option in type_options:
+		type_labels[type_option.name] = type_option.name.replace("-option", "")
 
 func show_at(pos : Vector2):
 	position = pos
@@ -18,26 +24,44 @@ func close_and_hide():
 
 func open():
 	is_open = true
-	$"BButton/HighlightCircle".hide()
 	$AButton.show()
 	$MenuArrow.show()
 	$BackdropCircle.show()
+	$Label.show()
+	$TypeMenu.show()
+
+func d_ang(angle1, angle2):
+	var diff = angle2 - angle1
+	return abs(diff) if abs(diff) < 180 else abs(diff + (360 * -sign(diff)))
 
 func rotate_arrow(angle : float):
 	$MenuArrow.rotation = angle
 
+
 func close():
 	is_open = false
-	$"BButton/HighlightCircle".show()
 	$AButton.hide()
 	$MenuArrow.hide()
 	$BackdropCircle.hide()
+	$Label.hide()
+	$TypeMenu.hide()
 
 func align():
 	position.x = MENU_RADIUS if position.x < MENU_RADIUS else position.x
 	position.y = MENU_RADIUS if position.y < MENU_RADIUS else position.y
 	position.x = get_viewport().size.x - MENU_RADIUS if position.x + MENU_RADIUS > get_viewport().size.x else position.x
 	position.y = get_viewport().size.y - MENU_RADIUS if position.y + MENU_RADIUS > get_viewport().size.y else position.y
+	var pointing_at = type_options[1]
+	var min_delta = 361.0
+	for opt in type_options:
+		var delta = d_ang(rad_to_deg($MenuArrow.rotation), rad_to_deg(Vector2.ZERO.angle_to_point(opt.position)))
+		opt.material.set("shader_parameter/blink", false)
+		if delta < min_delta:
+			min_delta = delta
+			pointing_at = opt
 
-func _process(delta):
+	pointing_at.material.set("shader_parameter/blink", true)
+	$Label.text = type_labels[pointing_at.name]
+
+func _process(_delta):
 	align()
