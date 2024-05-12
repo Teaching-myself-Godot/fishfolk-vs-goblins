cc5ce82a6b41da9d5c0e4034c1ff9104a389960d
rene.vanderark@kb.nl <rene.vanderark@kb.nl>
2024-05-12 14:52:39 +0200

    extracting motion code for new monster.

--- scripts/base_monster.gd
+++ scripts/base_monster.gd
@@ -4,7 +4,7 @@ extends Area3D
 var chest_height = 0.75
 var target : PathFollow3D = null
 var speed = 1.0
-var bounce_velocity_on_damage = 45
+var bounce_velocity_on_damage = 45.0
 var velocity = Vector3.ZERO
 var flying = false
 
@@ -13,27 +13,14 @@ var is_on_floor = false
 signal drop_magical_crystal(pos : Vector3)
 signal drop_builder_gem(pos : Vector3)
 
+func _apply_motion(delta):
+	printerr("_apply_motion(...) should be overridden!")
+
+func _apply_damage_motion(from_direction : Vector3, force : float = 1.0):
+	printerr("_apply_damage_motion(...) should be overridden!")
+
 func _physics_process(delta):
-	var direction = position.direction_to(target.position)
-
-	if flying or $HPBar.hp <= 0:
-		velocity.y = lerp(velocity.y, -gravity, 0.1)
-		velocity.x = lerp(velocity.x, 0.0, 0.05)
-		velocity.z = lerp(velocity.z, 0.0, 0.05)
-		$Armature.rotation.x = lerp_angle($Armature.rotation.x, $Armature.rotation.x + randf(), 0.25)
-		$Armature.rotation.y = lerp_angle($Armature.rotation.y, $Armature.rotation.y + .5, 0.25)
-	elif $HPBar.hp > 0:
-		if position.distance_to(target.position) < speed * 2:
-			target.progress += delta * speed
-		velocity.x = lerp(velocity.x, direction.x * speed, 0.25)
-		velocity.z = lerp(velocity.z, direction.z * speed, 0.25)
-		if is_on_floor:
-			velocity.y = lerp(velocity.y, direction.y * speed, 0.1)
-		else:
-			velocity.y -= delta * gravity * 3
-		$Armature.rotation.x = lerp_angle($Armature.rotation.x, 0, 0.25)
-		$Armature.rotation.y = lerp_angle($Armature.rotation.y, atan2(direction.x, direction.z), 0.05)
-
+	_apply_motion(delta)
 	$HPBar.position = CameraUtil.get_label_position(position, Vector3(-1.0, 2.2, 1.0))
 	position += velocity * delta
 
@@ -51,24 +38,18 @@ func take_damage(damage : int, from_direction : Vector3, force : float = 1.0):
 	$HPBar.hp -= actual_damage 
 	$HPBar.draw_damage(actual_damage)
 	$HPBar.queue_redraw()
-	velocity.y = bounce_velocity_on_damage * force
-	velocity.x = from_direction.x * 2 * force
-	velocity.z = from_direction.z * 2 * force
-	$Armature.rotation.y = -$Armature.rotation.y
-	flying = true
-	add_to_group(Constants.GROUP_NAME_MONSTERS_AIRBORNE)
-
-
-func _on_body_exited(body):
-	if body.is_in_group(Constants.GROUP_NAME_TERRAIN):
-		is_on_floor = false
-
+	_apply_damage_motion(from_direction, force)
 
 func _drop_gem():
 	if randf() < 0.5:
 		drop_builder_gem.emit(position)
 	elif randf() < 0.25:
 		drop_magical_crystal.emit(position)
+
+
+func _on_body_exited(body):
+	if body.is_in_group(Constants.GROUP_NAME_TERRAIN):
+		is_on_floor = false
 
 
 func _on_body_entered(body):
--- scripts/fish_chibi.gd
+++ scripts/fish_chibi.gd
@@ -1,6 +1,35 @@
 class_name FishChibi
 extends BaseMonster
 
+func _apply_motion(delta):
+	var direction = position.direction_to(target.position)
+	if flying or $HPBar.hp <= 0:
+		velocity.y = lerp(velocity.y, -gravity, 0.1)
+		velocity.x = lerp(velocity.x, 0.0, 0.05)
+		velocity.z = lerp(velocity.z, 0.0, 0.05)
+		$Armature.rotation.x = lerp_angle($Armature.rotation.x, $Armature.rotation.x + randf(), 0.25)
+		$Armature.rotation.y = lerp_angle($Armature.rotation.y, $Armature.rotation.y + .5, 0.25)
+	elif $HPBar.hp > 0:
+		if position.distance_to(target.position) < speed * 2:
+			target.progress += delta * speed
+		velocity.x = lerp(velocity.x, direction.x * speed, 0.25)
+		velocity.z = lerp(velocity.z, direction.z * speed, 0.25)
+		if is_on_floor:
+			velocity.y = lerp(velocity.y, direction.y * speed, 0.1)
+		else:
+			velocity.y -= delta * gravity * 3
+		$Armature.rotation.x = lerp_angle($Armature.rotation.x, 0, 0.25)
+		$Armature.rotation.y = lerp_angle($Armature.rotation.y, atan2(direction.x, direction.z), 0.05)
+
+
+func _apply_damage_motion(from_direction : Vector3, force : float = 1.0):
+	velocity.y = bounce_velocity_on_damage * force
+	velocity.x = from_direction.x * 2 * force
+	velocity.z = from_direction.z * 2 * force
+	$Armature.rotation.y = -$Armature.rotation.y
+	flying = true
+	add_to_group(Constants.GROUP_NAME_MONSTERS_AIRBORNE)
+
 
 func _ready():
 	super._ready()
