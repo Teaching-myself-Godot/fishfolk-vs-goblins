06ca9fd97d54d68ce34cc78968c2814c6fb1ba17
rene.vanderark@kb.nl <rene.vanderark@kb.nl>
2024-06-06 19:47:36 +0200

    adds attack and gameover.

---
+++ scenes_2d/main_menus/scripts/game_over_splash.gd
@@ -0,0 +1,25 @@
+class_name GameOverSplash
+extends Sprite2D
+
+
+signal close_gameover_splash();
+
+
+func _ready():
+	get_tree().get_root().size_changed.connect(_on_resize)
+	_on_resize()
+
+
+func _unhandled_input(_event):
+	if not visible:
+		return
+
+	if Input.is_anything_pressed():
+		close_gameover_splash.emit()
+
+
+func _on_resize():
+	texture.width = get_viewport().size.x
+	texture.height = get_viewport().size.y
+	$Label.size = get_viewport().size
+
