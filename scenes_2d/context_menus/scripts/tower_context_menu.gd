d21343528ed9549123736fb9c2eda663a934e5a0
rene.vanderark@kb.nl <rene.vanderark@kb.nl>
2024-07-14 14:43:47 +0200

    Adds tower context menu scene.

---
+++ scenes_2d/context_menus/scripts/tower_context_menu.gd
@@ -0,0 +1,12 @@
+class_name TowerContextMenu
+extends ContextMenuBase
+
+const INITIAL_OPTION = "Dismantle" 
+
+func _ready():
+	_init_menu(MAIN_MENU_NAME)
+	initial_option = INITIAL_OPTION
+
+
+func _on_gem_pouch_contents_changed(gems : int, crystals : int):
+	$"Dismantle-option/PriceTag".update_affordable_status(gems, crystals)
