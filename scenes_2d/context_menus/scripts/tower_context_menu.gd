bc0f4729416e0576f17944d0be9af9ae978fd758
rene.vanderark@kb.nl <rene.vanderark@kb.nl>
2024-07-15 22:37:44 +0200

    adds range unimplemented range upgrade option.

--- scenes_2d/context_menus/scripts/tower_context_menu.gd
+++ scenes_2d/context_menus/scripts/tower_context_menu.gd
@@ -1,7 +1,7 @@
 class_name TowerContextMenu
 extends ContextMenuBase
 
-const INITIAL_OPTION = "Dismantle" 
+const INITIAL_OPTION = "Range Upgrade"
 
 func _ready():
 	_init_menu(MAIN_MENU_NAME)
@@ -10,3 +10,4 @@ func _ready():
 
 func _on_gem_pouch_contents_changed(gems : int, crystals : int):
 	$"MainMenu/Dismantle-option/PriceTag".update_affordable_status(gems, crystals)
+	$"MainMenu/Range Upgrade-option/PriceTag".update_affordable_status(gems, crystals)
