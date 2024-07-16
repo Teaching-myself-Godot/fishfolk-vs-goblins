33f7a17f4176f8c0432ce00eef9e49020b1ff204
rene.vanderark@kb.nl <rene.vanderark@kb.nl>
2024-07-16 14:46:41 +0200

    adds damage upgrade icon.

--- scenes_2d/context_menus/scripts/tower_context_menu.gd
+++ scenes_2d/context_menus/scripts/tower_context_menu.gd
@@ -12,3 +12,4 @@ func _on_gem_pouch_contents_changed(gems : int, crystals : int):
 	$"MainMenu/Dismantle-option/PriceTag".update_affordable_status(gems, crystals)
 	$"MainMenu/Range-option/PriceTag".update_affordable_status(gems, crystals)
 	$"MainMenu/Rate of Fire-option/PriceTag".update_affordable_status(gems, crystals)
+	$"MainMenu/Damage-option/PriceTag".update_affordable_status(gems, crystals)
