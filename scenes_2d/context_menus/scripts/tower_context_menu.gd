6c041730fd4d51cdd3fd2374297b84f2935b7016
rene.vanderark@kb.nl <rene.vanderark@kb.nl>
2024-07-16 15:01:28 +0200

    moves upgrade options to upgrade submenu.

--- scenes_2d/context_menus/scripts/tower_context_menu.gd
+++ scenes_2d/context_menus/scripts/tower_context_menu.gd
@@ -1,15 +1,25 @@
 class_name TowerContextMenu
 extends ContextMenuBase
 
-const INITIAL_OPTION = "Range"
+const INITIAL_OPTION = "Upgrade"
 
 func _ready():
 	_init_menu(MAIN_MENU_NAME)
+	_init_menu("Upgrade")
 	initial_option = INITIAL_OPTION
+
+func open():
+	super.open()
+	$UpgradeMenu.hide()
+
+
+func close():
+	super.close()
+	$UpgradeMenu.hide()
 
 
 func _on_gem_pouch_contents_changed(gems : int, crystals : int):
 	$"MainMenu/Dismantle-option/PriceTag".update_affordable_status(gems, crystals)
-	$"MainMenu/Range-option/PriceTag".update_affordable_status(gems, crystals)
-	$"MainMenu/Rate of Fire-option/PriceTag".update_affordable_status(gems, crystals)
-	$"MainMenu/Damage-option/PriceTag".update_affordable_status(gems, crystals)
+	$"UpgradeMenu/Range-option/PriceTag".update_affordable_status(gems, crystals)
+	$"UpgradeMenu/Rate of Fire-option/PriceTag".update_affordable_status(gems, crystals)
+	$"UpgradeMenu/Damage-option/PriceTag".update_affordable_status(gems, crystals)
