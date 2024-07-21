519bf932f745226d2eb3a819cdfb1227f7f33f7b
rene.vanderark@kb.nl <rene.vanderark@kb.nl>
2024-07-21 15:24:47 +0200

    tweaks arrow.

--- scenes_2d/hud/scripts/arrow.gd
+++ scenes_2d/hud/scripts/arrow.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-const ARROW_POINT_WIDTH = 16
+const ARROW_POINT_WIDTH = 50
 
 @export var from : Vector2
 @export var to : Vector2
@@ -14,21 +14,16 @@ func _process(_delta):
 	
 	
 	$PointFill.offset.x = size - ARROW_POINT_WIDTH
-	$Fill.points[1].x = size - ARROW_POINT_WIDTH
-	$Fill.points[2].x = size - ARROW_POINT_WIDTH
-	$Fill.points[3].x = size
-	$Fill.points[4].x = size - ARROW_POINT_WIDTH
-	$Fill.points[5].x = size - ARROW_POINT_WIDTH
-	$Outline.points[1].x = size - ARROW_POINT_WIDTH - 1
-	$Outline.points[2].x = size - ARROW_POINT_WIDTH - 2
-	$Outline.points[3].x = size + 2
-	$Outline.points[4].x = size - ARROW_POINT_WIDTH - 2
-	$Outline.points[5].x = size - ARROW_POINT_WIDTH - 1
-
+	$Outline.points[1].x = size - ARROW_POINT_WIDTH + 2
+	$Outline.points[2].x = size - ARROW_POINT_WIDTH
+	$Outline.points[3].x = size + 3
+	$Outline.points[4].x = size - ARROW_POINT_WIDTH
+	$Outline.points[5].x = size - ARROW_POINT_WIDTH + 2
+	$LineFill.scale.x = size - ARROW_POINT_WIDTH + 2
 
 	position = from
 	rotation = from.angle_to_point(to)
 	$PointFill.modulate = color
-	$Fill.default_color = color
+	$LineFill.modulate = color
 	$Outline.default_color = outline_color
 	queue_redraw()
