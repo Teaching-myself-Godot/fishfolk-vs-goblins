ee59135715f2fd18007c60265ec466f43359fc59
rene.vanderark@kb.nl <rene.vanderark@kb.nl>
2024-07-27 19:59:54 +0200

    Adds range ring and timeout to indicator.

--- scenes_2d/hud/scripts/indicator.gd
+++ scenes_2d/hud/scripts/indicator.gd
@@ -8,6 +8,8 @@ extends Control
 @export var target_2d : Vector2 = Vector2.ZERO
 @export var radius_3d : float = 0.0
 
+var _range_ring : RangeRing = null
+
 func _ready():
 	modulate.a = 0.0
 	$PointingArrow.to = target_2d
@@ -18,7 +20,6 @@ func _process(_delta: float) -> void:
 		size.x * 0.5,
 		size.y - 3
 	)
-
 	var global_to = (
 		CameraUtil.keep_in_viewport(
 				CameraUtil.get_label_position(target.position, Vector3.UP * height_3d)
@@ -29,6 +30,9 @@ func _process(_delta: float) -> void:
 	$Circle.position = global_from - position
 	$PointingArrow.to = global_to - position
 	$PointingArrow.from = global_from - position
+	if _range_ring and is_instance_valid(target):
+		_range_ring.position = target.position
+		TerrainShaderParams.range_rings_changed.emit()
 
 	if fading:
 		if modulate.a > 0.0:
@@ -41,4 +45,14 @@ func _process(_delta: float) -> void:
 			modulate.a += 0.05
 
 
+func start(duration : float = -1):
+	fading = false
+	$FadeoutTimeoutTimer.start(duration)
+	if radius_3d > 0 and is_instance_valid(target):
+		_range_ring = RangeRing.new(target.position, radius_3d)
+		TerrainShaderParams.add_range_ring(_range_ring)
 
+
+func _on_fadeout_timeout_timer_timeout() -> void:
+	fading = true
+	TerrainShaderParams.drop_range_ring(_range_ring)
--- scenes_3d/stages/scripts/tutorial.gd
+++ scenes_3d/stages/scripts/tutorial.gd
@@ -19,7 +19,7 @@ func _process(_delta):
 	if _awaiting_goblin:
 		_goblin = goblin_map[main_player_cid]
 		_awaiting_goblin = false
-		$TutorialPlaybook.show_next_indicator_3d(_goblin, 2.2)
+		$TutorialPlaybook.show_next_indicator_3d(_goblin, 2.2, 1)
 
 
 func _add_goblin_to_scene(num : int, start_pos : Vector3 = Vector3(0, 4, 2)):
@@ -57,6 +57,7 @@ func _ready():
 	super._ready()
 	first_wave = $MonsterSpawner/ChibiWave1
 	$GemPouch.remove_from_group(Constants.GROUP_NAME_HUD_ITEM)
+
 
 func _destroy_arrow() -> void:
 	for arrow in get_tree().get_nodes_in_group(Constants.GROUP_NAME_ARROW_2D):
