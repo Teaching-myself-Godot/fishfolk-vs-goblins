0a809dee83085d72d893f4507d7b19335f9f82b7
René van der Ark <r.van.der.ark@gmail.com>
2024-08-25 15:08:50 +0200

    prints elapsed time in survival mode.

--- scenes_3d/stages/scripts/scores.gd
+++ scenes_3d/stages/scripts/scores.gd
@@ -2,7 +2,24 @@ class_name Scores
 
 extends PanelContainer
 
-#TODO keep track of elapsed time
+@onready var survival_time_label = $VBoxContainer/PanelContainer/SurvivalTime
+
+var _elapsed_time = 0.0
 
 func _process(delta: float) -> void:
-	print(delta)
+	_elapsed_time += delta
+
+
+func _on_time_elapsed_timer_timeout() -> void:
+	var seconds = _elapsed_time - (floori(_elapsed_time / 60) * 60)
+	var minutes = floori(_elapsed_time / 60) % 60
+	var hours = floori(_elapsed_time / (60 * 60)) % 24
+	#print("%.*f" % [2, milis])
+
+	survival_time_label.text = (
+		" Survival time: " +
+		"%0*d:" % [2, hours] +
+		"%0*d:" % [2, minutes] +
+		"%0*.*f " % [4, 1, seconds]
+	)
+
